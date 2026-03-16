759769474764da0fa933ddbf1087d37d9cbb7e69
RichardZ24 <134828914+RichardZ24@users.noreply.github.com>
2026-03-16 17:29:45 +1100

    feat: add CLI path parsing and directory existance validation

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+from pathlib import Path
+import argparse
+
+#python3 main.py ~/test_folder_basic
+
 def main():
     print("Python CLI File Organiser!")
+
+    parser = argparse.ArgumentParser(description = "Python CLI File Organiser") 
+
+    #arguments needed
+    parser.add_argument("folder", help = "<- folder to organise")
+
+    args = parser.parse_args()
+    folder_to_organise = Path(args.folder)
+
+    if not (folder_to_organise.is_dir()):
+        print("ERROR: Provided path is not a valid directory.")
+        return
+    
+    
+    
     
 if __name__ == "__main__":
     main()
